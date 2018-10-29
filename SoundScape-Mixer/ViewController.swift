31c6fce3e321b8a9e13182061d0591b6973fdb8d
NGUYEN BA LONG <Long.Nguyen@metropolia.fi>
2018-10-29 11:19:47 +0200

    Initial Commit

---
+++ SoundScape-Mixer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundScape-Mixer
+//
+//  Created by Long Nguyen on 29/10/2018.
+//  Copyright © 2018 Long Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
