97edf22cb70ab5e8ee1c5a4626729dec0a8e360a
NGUYEN BA LONG <Long.Nguyen@metropolia.fi>
2018-11-26 01:37:42 +0200

    Display recordings in profileVC

--- SoundScape-Mixer/Screens/ProfileViewController.swift
+++ SoundScape-Mixer/Screens/ProfileViewController.swift
@@ -1,14 +1,73 @@
 import UIKit
 
+public let kRecordingCell: String = "kRecordingCell"
+
 class ProfileViewController: BaseViewController {
+    
+    private var recordings: [String] = [String]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override init(appController: AppController) {
         super.init(appController: appController)
         title = "Profile"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: kRecordingCell)
+        
+        tableView.delegate = self
+        
+        tableView.dataSource = self
+        
+        tableView.backgroundColor = .clear
+        
+        getAllRecordings()
     }
 
     required init?(coder _: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
     @IBAction func logoutButtonPressed(_: UIButton) {
         appController.logout()
+    }
+    
+    private func getAllRecordings() {
+        
+        do {
+            let contentOfRecordsFolder = try FileManager.default.contentsOfDirectory(atPath: getDocumentDirectory().relativePath)
+            
+            self.recordings = contentOfRecordsFolder
+        } catch let err {
+            print(err.localizedDescription)
+        }
+        
+    }
+    
+    private func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        print(paths[0])
+        
+        return paths[0].appendingPathComponent("Resources/Records/")
+    }
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recordings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kRecordingCell, for: indexPath)
+        
+        cell.textLabel?.text = recordings[indexPath.row]
+        
+        return cell
     }
 }
