ab7d09e263c60db38661fb9ea9b49f9eafcdc942
NGUYEN BA LONG <Long.Nguyen@metropolia.fi>
2018-12-12 21:53:36 +0200

    Implement test case for AudioLibraryCollectionManager

--- SoundScape-MixerTests/SoundScape_MixerTests.swift
+++ SoundScape-MixerTests/SoundScape_MixerTests.swift
@@ -9,26 +9,32 @@
 import XCTest
 @testable import SoundScape_Mixer
 
-class SoundScape_MixerTests: XCTestCase {
+public let kRandomCollectionName = "RandomCollectionName"
 
+class SoundScape_MixerTests: XCTestCase {
+    
+    var audioCollectionManager: AudioLibraryCollectionManager!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        audioCollectionManager = AudioLibraryCollectionManager.shared
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        audioCollectionManager = nil
+        
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSaveRandomCollection() {
+        audioCollectionManager.save(collectionName: kRandomCollectionName)
+        
+        let savedCollectionName = audioCollectionManager.collectionName ?? ""
+        
+        XCTAssertEqual(savedCollectionName, kRandomCollectionName, "Value saved from AudioLibraryCollectionManager is wrong!")
     }
 
 }
